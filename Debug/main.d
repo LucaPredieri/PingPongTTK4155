7f7f82b450caa224f621d173ffdd02ecfb12c2b5
Luca Predieri <lucapre@ntnu.no>
2022-09-19 17:25:59 +0200

    Exercise 3 almost done, tided some things up.

--- Debug/main.d
+++ Debug/main.d
@@ -7,7 +7,7 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
- .././Uart_driver.h \
+ .././Uart_driver.h .././usart_driver.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
  C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom162.h \
@@ -19,7 +19,8 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sleep.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ .././xmem_driver.h .././ADC.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
 
@@ -38,6 +39,8 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
 
 .././Uart_driver.h:
+
+.././usart_driver.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -62,3 +65,7 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+.././xmem_driver.h:
+
+.././ADC.h:
--- main.c
+++ main.c
@@ -4,108 +4,42 @@
 #include <stdio.h>
 #include <string.h>
 #include "Uart_driver.h"
+#include "usart_driver.h"
+#include "xmem_driver.h"
+#include "ADC.h"
 #include <avr/io.h>
 #include <avr/sleep.h>
 #include <util/delay.h>
 
-#define BASE_ADDRESS_OLED 0x1000
-#define BASE_ADDRESS_ADC 0x1400
-#define BASE_ADDRESS_SRAM 0x1800
-#define SINGLE_CHANNEL_SAMPLE 7
+struct joy_pos {   // Structure declaration
+  uint8_t x_pos;           
+  uint8_t y_pos;       
+};
 
-// this function is called by printf as a stream handler
-int usart_putchar_printf(char var, FILE *stream) {
-    // translate \n to \r for br@y++ terminal
-    if (var == '\n') usart_putchar('\r');
-    usart_putchar(var);
-    return 0;
+struct slider_pos {   // Structure declaration
+  uint8_t left;           
+  uint8_t right;       
+};
+
+struct joy_pos joystick;
+struct slider_pos slider;
+
+
+struct joy_pos get_joypos(){
+	joystick.x_pos = xmem_read(0x00, BASE_ADDRESS_ADC)*100/255;
+	joystick.y_pos = xmem_read(0x00, BASE_ADDRESS_ADC)*100/255;	
+	xmem_read(0x00, BASE_ADDRESS_ADC);
+    xmem_read(0x00, BASE_ADDRESS_ADC);
+	return joystick;		
 }
 
-void xmem_init ( void ){
-	MCUCR |= (1 << SRE ); // enable XMEM
-	SFIOR |= (1 << XMM2 ); // mask unused bits
-}
 
-void xmem_write ( uint8_t data , uint16_t addr, uint16_t base){
-	volatile char * ext_mem = ( char *) base ;
-	ext_mem [ addr ]= data ;
-}
-uint8_t xmem_read ( uint16_t addr, uint16_t base ){
-	volatile char * ext_mem = ( char *) base;
-	uint8_t ret_val = ext_mem [ addr ];
-	return ret_val ;
-}
-
-void usart_putchar(char data) {
-    // Wait for empty transmit buffer
-    while(!(UCSR0A & (1<<UDRE0))){
-			//While the flag is off
-		}
-		//send message
-		UDR0 = data;
-}
-
-void PWM_init(){
-	DDRD |= (1 << DDD4 );
-	TCCR3A |= (0 << WGM31) | (0 << WGM30) | (1 << COM3A0);
-	TCCR3B |= (0 << WGM33) | (1 << WGM32) | (0 << CS32) | (0 << CS31) | (1 << CS30) ;
-	OCR3A = 0;
-}
-
-static FILE mystdout = FDEV_SETUP_STREAM(usart_putchar_printf, NULL, _FDEV_SETUP_WRITE);
-
-void SRAM_test(void)
-{
-	volatile char *ext_ram = (char *) 0x1800; // Start address for the SRAM
-    uint16_t ext_ram_size = 0x800;
-    uint16_t write_errors = 0;
-    uint16_t retrieval_errors = 0;
-    printf("Starting SRAM test...\n");
-    // rand() stores some internal state, so calling this function in a loop will
-    // yield different seeds each time (unless srand() is called before this function)
-    uint16_t seed = rand();
-    // Write phase: Immediately check that the correct value was stored
-    srand(seed);
-    for (uint16_t i = 0; i < ext_ram_size; i++) {
-        uint8_t some_value = rand();
-        ext_ram[i] = some_value;
-        uint8_t retreived_value = ext_ram[i];
-        if (retreived_value != some_value) {
-            printf("Write phase error: ext_ram[%4d] = %02X (should be %02X)\n", i, retreived_value, some_value);
-            write_errors++;
-        }
-    }
-    // Retrieval phase: Check that no values were changed during or after the write phase
-    srand(seed);
-    // reset the PRNG to the state it had before the write phase
-    for (uint16_t i = 0; i < ext_ram_size; i++) {
-        uint8_t some_value = rand();
-        uint8_t retreived_value = ext_ram[i];
-        if (retreived_value != some_value) {
-            printf("Retrieval phase error: ext_ram[%4d] = %02X (should be %02X)\n", i, retreived_value, some_value);
-            retrieval_errors++;
-        }
-    }
-    printf("SRAM test completed with \n%4d errors in write phase and \n%4d errors in retrieval phase\n\n", write_errors, retrieval_errors);
-}
-
-void uart_init(unsigned int baud){
-	int bd = 31;
-	//set baud rate
-	UBRR0H = (bd>>8);
-	UBRR0L = bd;
-	//Enable r/t
-	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
-	//set frame format
-	UCSR0C = (1<<USBS0) | (1<<URSEL0) | (3<<UCSZ00);
-	fdevopen(usart_putchar_printf,uart_receive);
-}
-
-int uart_receive(){
-	if(!(UCSR0A & (1<<RXC0))){
-		return 0;
-	}
-	return UDR0;
+struct slider_pos get_sliderpos(){
+	xmem_read(0x00, BASE_ADDRESS_ADC);
+    xmem_read(0x00, BASE_ADDRESS_ADC);
+	slider.left = xmem_read(0x00, BASE_ADDRESS_ADC)*100/255;
+	slider.right = xmem_read(0x00, BASE_ADDRESS_ADC)*100/255;
+	return slider;			
 }
 
 int main(){
@@ -119,13 +53,22 @@ int main(){
 	uint16_t seed;
 	addre1=0;
 	printf("///////////////\n");
+	uint8_t value = 0;
+	DDRB = 0x00;
+
 	while(1){
+		/*value = PINB;
 		uint8_t adc_mode = (1 << SINGLE_CHANNEL_SAMPLE) | (0);
 
 		xmem_write(adc_mode, 0x01, BASE_ADDRESS_ADC);
 		_delay_us(5000);
-		uint8_t value1 = xmem_read(0x00, BASE_ADDRESS_ADC);
-		uint8_t value2 = xmem_read(0x00, BASE_ADDRESS_ADC);
-		printf("value1 = %d value2 = %d \n ", value1, value2);
-	}
+		joystick = get_joypos();
+		slider=get_sliderpos();
+		printf("xjoy= %d yjoy= %d left= %d right = %d \n",joystick.x_pos, joystick.y_pos, slider.left, slider.right);*/
+		SRAM_test();
+	} 
 }
+
+
+
+
